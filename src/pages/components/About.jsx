d823232b1ac44f0d3b1c8f90ea413690f4f3fb2a
Julian Grande <juliangrande11@gmail.com>
2023-12-30 20:03:55 -0500

    Timeline started and working, maybe adjust About page layout so that it isnt so tight, brainstorm color scheme?

--- src/pages/components/About.jsx
+++ src/pages/components/About.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Image from 'next/image';
 import placeholder from '../../../public/my_images/Specialty_Pizza.jpg'
+import Timeline from './Timeline';
 
 const About = () => {
     return (
@@ -8,25 +9,28 @@ const About = () => {
         <div id='about' className="w-full md:h-screen p-2 flex items-center py-16">
             <div className="max-w-[1240px] m-auto md:grid grid-cols-3 gap-8">
                 <div className="col-span-2">
-                    <h3 className="uppercase text-xl tracking-widest text-[#800000] text-left">
+                    <h2 className="uppercase text-xl tracking-widest text-[#800000] text-center">
                         About
-                    </h3>
+                    </h2>
 
-                    <p className="py-2 text-gray-700">
-                        About text goes here
+                    <p className="py-2 text-gray-700 text-center">
+                        About text goes here About text goes here About text goes here
+                        About text goes here About text goes here About text goes here
+                        About text goes here About text goes here About text goes here
+                        About text goes here About text goes here About text goes here
                     </p>
 
-                    <p className="py-2 text-gray-700">
-                        Second paragraph maybe?
+                    <p className="py-2 text-gray-700 text-center">
+                    Second paragraph maybe? Second paragraph maybe?
+                    Second paragraph maybe? Second paragraph maybe?
+                    Second paragraph maybe? Second paragraph maybe?
                     </p>
                 </div>
                 <div className="w-full h-auto m-auto shadow-s shadow-gray-700 rounded-xl flex items-center justify-center p-4 hover:scale-75 ease-in duration-200">
                     <Image src={placeholder} className="rounded-xl" alt='/'></Image>
                 </div>
-                <div className="w-full h-auto m-auto flex items-center justify-center">
-                    //Timeline here
-                </div>
             </div>
+            <Timeline />
         </div>
 
     );
---
+++ src/pages/components/Timeline.jsx
@@ -0,0 +1,39 @@
+const Timeline = () => {
+    return (
+
+        <div className="scale-[65%] md:scale-100 w-10/12 md:w-7/12 lg:w-6/12 mx-auto my-10">
+            {/* <h3 className="text-center text-[#800000]">
+                My Career
+            </h3> */}
+
+            <div className="border-l-2 mt-10">
+
+                {/* Card 1 */}
+                <div className="transform transition cursor-pointer hover:-translate-y-2 ml-10 relative flex items-center px-6 bg-[#B32B48] text-gray-700 rounded mb-10 flex-col md:flex-row space-y-4 md:space-y-0">
+                    {/* Dot */}
+                    <div className="w-5 h-5 bg-[#B32B48] absolute -left-10 transform -translate-x-2/4 rounded-full z-10 mt-2 md:mt-0">
+                    </div>
+
+                    {/* Line */}
+                    <div className="w-10 h-1 bg-black absolute -left-10 z-0">
+                    </div>
+
+                    {/* Content */}
+                    <div className="flex-auto">
+                        <h3>Hack4Impact - Rutgers, NJ</h3>
+                        <h3>Backend Team Member</h3>
+                        <p>Spring 2023</p>
+                    </div>
+
+                    <section href="#" className="text-center text-gray-700">Full Stack</section>
+                </div>
+
+
+                {/* Card 2*/}
+            </div>
+        </div>
+
+    );
+};
+
+export default Timeline;
